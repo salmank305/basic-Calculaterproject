f709fbfce4b78cedfe9ecfda1a3ff0bdd2416219
salman305 <salmankhatik300591@gmail.com>
2022-11-28 14:04:35 +0530

    css

--- src/Basic Calculater/Calculater.jsx
+++ src/Basic Calculater/Calculater.jsx
@@ -5,7 +5,7 @@ import "./Calculate.css";
 export const Calculater = () => {
     const [result,setResult]=useState('');
     const handler=(e)=>{
-      setResult(result.concat(e.target.name)+" ")
+        setResult(result.concat(e.target.name))
     }
     const clear=()=>{
       setResult('')
@@ -27,29 +27,29 @@ export const Calculater = () => {
    
     <>
      <h1>Calculater</h1>
-     <div className='container'>
+     <div className='container' >
         <form>
             <input type="text" value={result} />
         </form>
         <div className='keypad'>
-            <button onClick={clear} id="clear">Clear</button>
+            <button className="clr" onClick={clear} id="clear">Clear</button>
             <button onClick={backspace}>C</button>
-            <button name="/" onClick={handler}>&divide;</button>           
+            <button className='mathOp' name="/" onClick={handler}>&divide;</button>           
             <button name="1" onClick={handler}>1</button>
             <button name="2" onClick={handler}>2</button>
             <button name="3" onClick={handler}>3</button>
-            <button name="*" onClick={handler}>&times;</button>           
+            <button className='mathOp' name="*" onClick={handler}>&times;</button>           
             <button name="4" onClick={handler}>4</button>
             <button name="5" onClick={handler}>5</button>
             <button name="6" onClick={handler}>6</button>
-            <button name="-" onClick={handler}>&ndash;</button>       
+            <button className='mathOp'name="-" onClick={handler}>&ndash;</button>       
             <button name="7" onClick={handler}>7</button>
             <button name="8" onClick={handler}>8</button> 
             <button name="9" onClick={handler}>9</button>
-            <button name="+" onClick={handler}>+</button>
-            <button name="." onClick={handler}>.</button>
+            <button className='mathOp' name="+" onClick={handler}>+</button>
+            <button className="dot" name="." onClick={handler}>.</button>
             <button name="0" onClick={handler}>0</button>          
-            <button  onClick={calculate} id="result">=</button> 
+            <button className="sum" onClick={calculate} id="result">=</button> 
         </div>
      </div>
 
